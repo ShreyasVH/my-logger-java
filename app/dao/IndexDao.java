64059b6dd92a8d54a36cacce6b63a82f9ef4d4a5
Shreyas <shreyas.hande@quikr.com>
2019-07-23 15:28:00 +0530

    removing interfaces for dao

--- app/dao/IndexDao.java
+++ app/dao/IndexDao.java
@@ -1,15 +1,96 @@
 package dao;
 
+import com.google.inject.Inject;
+import io.ebean.Ebean;
+import io.ebean.EbeanServer;
+import io.ebean.ExpressionList;
+import io.ebean.Query;
 import models.Log;
 
+import javax.persistence.OptimisticLockException;
 import java.util.Date;
 import java.util.List;
 
-public interface IndexDao
+public class IndexDao
 {
-    String index();
+     private static EbeanServer db;
 
-    List<Log> getWithFilters(List<String> types, List<String> sources, Date from, Date to, Integer offset, Integer limit);
+    @Inject
+    public IndexDao()
+    {
+//        if(null == db)
+//        {
+//            db = Ebean.getServer("default");
+//        }
+    }
 
-    boolean saveLog(Log log);
+    public String index()
+    {
+        return "INDEX FROM DAO";
+    }
+
+    public List<Log> getWithFilters(List<String> types, List<String> sources, Date from, Date to, Integer offset, Integer limit)
+    {
+        if(null == db)
+        {
+            db = Ebean.getServer("default");
+        }
+        ExpressionList<Log> expressionList = db.find(Log.class).where();
+
+        if(!types.isEmpty())
+        {
+            expressionList = expressionList.in("type", types);
+        }
+
+        if(!sources.isEmpty())
+        {
+            expressionList = expressionList.in("source", sources);
+        }
+
+        if(null != from)
+        {
+            expressionList = expressionList.ge("createdAt", from);
+        }
+
+        if(null != to)
+        {
+            expressionList = expressionList.le("createdAt", to);
+        }
+
+        Query<Log> query = expressionList.query();
+
+        if(null != offset)
+        {
+            query = expressionList.setFirstRow(offset);
+        }
+
+        if(null != limit)
+        {
+            query = query.setMaxRows(limit);
+        }
+
+        query.orderBy("id desc");
+
+        return query.findList();
+    }
+
+    public boolean saveLog(Log log)
+    {
+        if(null == db)
+        {
+            db = Ebean.getServer("default");
+        }
+        boolean isSuccess = false;
+        try
+        {
+            db.save(log);
+            isSuccess = true;
+        }
+        catch(OptimisticLockException ex)
+        {
+
+        }
+
+        return isSuccess;
+    }
 }
