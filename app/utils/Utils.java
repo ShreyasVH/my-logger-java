49f27c47c7abbad03ef709641b473ec51e8422ce
Shreyas <shreyas.hande@gmail.com>
2019-07-23 23:44:53 +0530

    added util function for current date

--- app/utils/Utils.java
+++ app/utils/Utils.java
@@ -3,7 +3,10 @@ package utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class Utils
 {
@@ -16,5 +19,14 @@ public class Utils
     public static <T> List<T> convertObjectList(Object from, Class<T> to)
     {
         return objMapper.convertValue(from, TypeFactory.defaultInstance().constructCollectionType(List.class, to));
+    }
+
+    public static Date getCurrentDate()
+    {
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.setTimeZone(TimeZone.getTimeZone("GMT+05:30"));
+
+        return c.getTime();
     }
 }
