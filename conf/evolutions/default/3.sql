7b10105f7d121db54051c7935b299fe5ced2a677
ShreyasVH <shreyas.hande@gmail.com>
2020-09-07 22:06:33 +0530

    corrected evolution scripts

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,2 +1,2 @@
-INSERT INTO `cron_logs` (`type`) VALUES (0);
 INSERT INTO `cron_logs` (`type`) VALUES (1);
+INSERT INTO `cron_logs` (`type`) VALUES (2);
--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -1,1 +1,1 @@
-INSERT INTO `cron_logs` (`type`) VALUES (2);
+INSERT INTO `cron_logs` (`type`) VALUES (3);
